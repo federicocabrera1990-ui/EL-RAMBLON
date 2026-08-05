20a80389d5573ee6f3a6742f8e232e1fc5c23473
federicocabrera1990-ui <federicocabrera1990@gmail.com>
2026-08-05 12:38:59 +0000

    5 ago 2026, 9:38 a.m.

--- Administrador/Backend/php/check.php
+++ Administrador/Backend/php/check.php
@@ -11,3 +11,4 @@ $log   = isset($_SESSION['log'], $_SESSION['rol']) && $_SESSION['log'] === true;
 $admin = $log && $_SESSION['rol'] == 'admin';
 
 echo json_encode(["status" => $log, "admin" => $admin]);
+asd
